f6633a042bd3038e56c38c0d2fe81d74a61012b3
Carson Full <carson.full@gmomail.org>
2014-08-01 09:41:08 -0500

    Added ServiceProvider and Keys for Dependency Injection

---
+++ src/CacheKeys.php
@@ -0,0 +1,12 @@
+<?php
+namespace GMO\Cache;
+
+final class CacheKeys {
+
+	const REDIS_HOST = 'redis.host';
+	const REDIS_PORT = 'redis.port';
+	const REDIS_NEW = 'redis.new';
+	const REDIS = 'redis';
+
+	private function __construct() { }
+}
---
+++ src/CacheServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace GMO\Cache;
+
+use GMO\DependencyInjection\ServiceProviderInterface;
+use Pimple;
+
+class CacheServiceProvider implements ServiceProviderInterface {
+
+	/** @inheritdoc */
+	public function register(Pimple $container) {
+
+		$container[CacheKeys::REDIS_HOST] = 'localhost';
+		$container[CacheKeys::REDIS_PORT] = 6379;
+
+		$container[CacheKeys::REDIS_NEW] = function() use ($container) {
+			return new Redis($container[CacheKeys::REDIS_HOST], CacheKeys::REDIS_PORT);
+		};
+		$container[CacheKeys::REDIS] = $container->share(function() use ($container) {
+			return $container[CacheKeys::REDIS_NEW];
+		});
+	}
+
+	/** @inheritdoc */
+	public function boot(Pimple $container) { }
+}
